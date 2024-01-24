5e5bc68b221081d3e95c476c7067e00a37d19770
JayvonK <jaykhuth@gmail.com>
2024-01-24 13:05:31 -0800

    builder services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using FavoriteColorDatabase.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DatabaseConnection"))
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
