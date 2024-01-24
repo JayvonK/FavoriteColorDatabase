e893d3e1052521b7533873728257b990368f3109
JayvonK <jaykhuth@gmail.com>
2024-01-24 13:21:44 -0800

    implemented interface

---
+++ Services/FavColor.cs
@@ -0,0 +1,16 @@
+using FavoriteColorDatabase.Models;
+
+namespace FavoriteColorDatabase.Services;
+
+public class FavColor : IFavColor
+{
+    public List<FavoriteColor> AddFavColor(string name, string color)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<FavoriteColor> GetData()
+    {
+        throw new NotImplementedException();
+    }
+}
