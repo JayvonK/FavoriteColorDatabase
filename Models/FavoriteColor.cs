c70e6fb9925e4357913018fc23c963cdf799bcfd
JayvonK <jaykhuth@gmail.com>
2024-01-24 12:41:21 -0800

    model

---
+++ Models/FavoriteColor.cs
@@ -0,0 +1,12 @@
+namespace FavoriteColorDatabase.Models;
+
+    public class FavoriteColor
+    {
+        public int Id { get; set; }
+
+        public string Color { get; set; } = "Unknown";
+
+        public string Name { get; set; } = "Unknown";
+
+        public int Age { get; set; }
+    }
